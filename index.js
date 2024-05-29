b25c7d1a6c207f596319ed3d3bce3ab996086b87
mumtozBEK1234 <email manzilingiz>
2024-05-29 11:52:12 +0300

    4-oy 6dars uy ishi

---
+++ index.js
@@ -0,0 +1,59 @@
+// 1 masala
+
+// let a = 50 
+// for(let i = 1 ; i <= 50 ; i+=2){
+//     console.log(i * i);
+// }
+
+// 2 masala 
+
+// let a = 10 
+
+// for(let i = 1 ; i <= 10 ; i++) {
+//     console.log(i * i * i);
+// }
+
+// 3 masla 
+
+// for (let i = 1; i <= 20; i++) {
+//     let resalt = 1;
+//     for (let a = 1; a <= i; a++) {
+//         resalt *= a;
+//     }
+//     console.log(resalt);
+// }
+
+// 4 masala 
+    // let a = 30 
+    // let resalt = 0
+    // for(let i = 1 ; i <= 30 ; i++ ) {
+    //    resalt += i*i ;
+    // }
+    // console.log( resalt);
+
+    // 5 masala 
+    // domla qila olmadim
+
+    
+    
+    // 6 masala 
+    // let a = 100 
+
+    // for(let i = 1 ; i <= a ; i++ ) {
+      
+    //     if( i * 2 < a )
+
+    //     console.log(i);
+    // }
+    
+    
+    // 7 - masla 
+
+//   let a = 100
+
+//   for (let i = 1; i <= 100; i++) {
+   
+//     if (i % 7 === 0 && i % 5 !== 0) {
+//         console.log(i);
+//     }
+// }
